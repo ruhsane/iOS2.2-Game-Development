5525982b6d42e5239503569493910d68a9a228c5
Rushani Shawuti <ruhsane924@gmail.com>
2019-11-11 10:52:48 -0800

    moving background implemented

--- SpaceJunk/SpaceJunk/GameScene.swift
+++ SpaceJunk/SpaceJunk/GameScene.swift
@@ -12,22 +12,46 @@ import UIKit
 
 class GameScene: SKScene {
     
-    let backgroundNode = SKSpriteNode(imageNamed: "vectorstock_16606572")
+//    let backgroundNode = SKSpriteNode(imageNamed: "vectorstock_16606572")
     let spaceShip = SKSpriteNode(imageNamed: "playerShip2_red")
     let explosionSound = SKAction.playSoundFileNamed("Explosion", waitForCompletion: true)
-
     var shipX = 100
+    var cameraNode: SKCameraNode!
+    let cameraMovePointsPerSec: CGFloat = 0.6
+    var playableRect: CGRect
+    var cameraRect : CGRect {
+      let x = cameraNode.position.x - size.width/2 + (size.width - playableRect.width)/2
+      let y = cameraNode.position.y - size.height/2 + (size.height - playableRect.height)/2
+      return CGRect(x: x, y: y, width: playableRect.width, height: playableRect.height)
+    }
+    
+    override init(size: CGSize) {
+        playableRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func didMove(to view: SKView) {
         
-        backgroundNode.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        backgroundNode.size = CGSize(width: self.frame.width, height: self.frame.height)
-        backgroundNode.zPosition = -1
-        self.addChild(backgroundNode)
+//        backgroundNode.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+//        backgroundNode.size = CGSize(width: self.frame.width, height: self.frame.height)
+//        backgroundNode.zPosition = -1
+//        self.addChild(backgroundNode)
+        createBackground()
         
         spaceShip.position = CGPoint(x: self.frame.midX , y: 50 )
         spaceShip.size = CGSize(width: 70, height: 50)
         self.addChild(spaceShip)
+        
+        // create camera
+        cameraNode = SKCameraNode()
+        addChild(cameraNode)
+        camera = cameraNode
+        cameraNode.position = CGPoint(x: size.width/2, y: size.height/2)
+
         
         endlessFall()
         dropBomb()
@@ -46,7 +70,7 @@ class GameScene: SKScene {
 
             let randomPositionX = CGFloat.random(in: 0..<self.size.width)
             bomb.position.x = randomPositionX
-            bomb.position.y = self.view!.bounds.height
+            bomb.position.y = self.cameraNode.position.y + 300
             bomb.size = CGSize(width: 100, height: 60)
 
             self.addChild(bomb)
@@ -75,7 +99,7 @@ class GameScene: SKScene {
 
                 let randomPositionX = CGFloat.random(in: 0..<self.size.width)
                 meteorites.position.x = randomPositionX
-                meteorites.position.y = self.view!.bounds.height
+                meteorites.position.y = self.cameraNode.position.y + 300
                 self.addChild(meteorites)
                 let group = SKAction.group([fallAction, rotateAction])
                 let sequence = SKAction.sequence([group, deleteAction])
@@ -87,7 +111,7 @@ class GameScene: SKScene {
 
                 let randomPositionX = CGFloat.random(in: 0..<self.size.width)
                 spaceDebris.position.x = randomPositionX
-                spaceDebris.position.y = self.view!.bounds.height
+                spaceDebris.position.y = self.cameraNode.position.y + 300
                 self.addChild(spaceDebris)
                 let group = SKAction.group([fallAction, rotateAction])
                 let sequence = SKAction.sequence([group, deleteAction])
@@ -111,8 +135,55 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         checkForCollision()
-        spaceShip.position = CGPoint(x: self.shipX , y: 50 )
+        moveCamera()
 
+    }
+    
+    func createBackground() {
+        for i in 0...1 {
+          let background = backgroundNode()
+          background.anchorPoint = CGPoint.zero
+          background.position = CGPoint(x: 0, y: CGFloat(i)*background.size.height)
+          background.name = "background"
+          background.zPosition = -1
+          addChild(background)
+        }
+    }
+    
+    func backgroundNode() -> SKSpriteNode {
+        // 1
+        let backgroundNode = SKSpriteNode()
+        backgroundNode.anchorPoint = CGPoint.zero
+        backgroundNode.name = "background"
+        // 2
+        let background1 = SKSpriteNode(imageNamed: "background")
+        background1.anchorPoint = CGPoint.zero
+        background1.position = CGPoint(x: 0, y: 0)
+        background1.size = UIScreen.main.bounds.size
+        backgroundNode.addChild(background1)
+        // 3
+        let background2 = SKSpriteNode(imageNamed: "background")
+        background2.anchorPoint = CGPoint.zero
+        background2.position = CGPoint(x: 0, y: background1.size.height)
+        background2.size = UIScreen.main.bounds.size
+        backgroundNode.addChild(background2)
+        // 4
+        backgroundNode.size = CGSize(width: background1.size.width , height: background1.size.height + background2.size.height)
+        return backgroundNode
+    }
+    
+    func moveCamera() {
+        cameraNode.position.y += cameraMovePointsPerSec
+        spaceShip.position.y = cameraNode.position.y - 300
+        spaceShip.position.x = CGFloat(self.shipX)
+        enumerateChildNodes(withName: "background") { node, _ in
+          let background = node as! SKSpriteNode
+          if background.position.y + background.size.height <
+            self.cameraRect.origin.y {
+            background.position = CGPoint(
+            x: background.position.x,
+            y: background.position.y + background.size.height*2) }
+        }
     }
 
 
